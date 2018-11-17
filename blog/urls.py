c1d4044239703ed951f0c5283cb302a95a5f2242
Icia-C <icia83@gmail.com>
2018-11-17 17:24:28 +0100

    added views to create/edit blog post inside the site

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,9 @@ from . import views
 urlpatterns = [
 	path('', views.post_list, name='post_list'),
 	path('post/<int:pk>/', views.post_detail, name='post_detail'),
+	path('post/new', views.post_new, name='post_new'),
+	path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
-
 
 #post/ significa que la URL debería empezar con la palabra post seguida por una /.
 #<int:pk> – Significa que Django buscará un número entero y se lo pasará a la vista en una variable llamada pk.
