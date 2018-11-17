4683b6ec5b273111799f629a1a9025e2ef670a09
Icia-C <icia83@gmail.com>
2018-11-17 16:48:17 +0100

    template details for the post

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
 	path('', views.post_list, name='post_list'),
+	path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
+
+
+#post/ significa que la URL debería empezar con la palabra post seguida por una /.
+#<int:pk> – Significa que Django buscará un número entero y se lo pasará a la vista en una variable llamada pk.
+# / – necesitamos otra / al final de la URL.
